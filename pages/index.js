b888d64ec11cb58d80cd0047df7d122f3d2d2382
yu2799 <ura.yu.2799@gmail.com>
2022-01-11 12:21:20 +0900

    add container

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import LineGraph from "../components/LineGraph";
 
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
 
 function Home() {
   const [selectedPrefecture, setSelectedPretecture] = useState("神奈川");
@@ -19,34 +20,36 @@ function Home() {
   };
 
   return (
-    <Grid container rowSpacing={3} columnSpacing={3}>
-      <Grid item xs={3}>
-        <Paper elevation={5}>
-          <JapanMap
-            changePrefecture={changePrefecture}
-            selectedPrefecture={selectedPrefecture}
-          />
-        </Paper>
+    <Container>
+      <Grid container rowSpacing={3} columnSpacing={3}>
+        <Grid item xs={3}>
+          <Paper elevation={5}>
+            <JapanMap
+              changePrefecture={changePrefecture}
+              selectedPrefecture={selectedPrefecture}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={9}>
+          <Paper elevation={5}>
+            <BarGraph
+              changePrefecture={changePrefecture}
+              selectedPrefecture={selectedPrefecture}
+              changeSchool={changeSchool}
+              selectedSchool={selectedSchool}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper elevation={5}>
+            <LineGraph
+              changeSchool={changeSchool}
+              selectedSchool={selectedSchool}
+            />
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={9}>
-        <Paper elevation={5}>
-          <BarGraph
-            changePrefecture={changePrefecture}
-            selectedPrefecture={selectedPrefecture}
-            changeSchool={changeSchool}
-            selectedSchool={selectedSchool}
-          />
-        </Paper>
-      </Grid>
-      <Grid item xs>
-        <Paper elevation={5}>
-          <LineGraph
-            changeSchool={changeSchool}
-            selectedSchool={selectedSchool}
-          />
-        </Paper>
-      </Grid>
-    </Grid>
+    </Container>
   );
 }
 
