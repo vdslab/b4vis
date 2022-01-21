1463d3e770c272ba826979f95e52420b207d6b8f
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2022-01-21 22:45:24 +0900

    ダイアログのサイズ変更、県表示ビューの説明

--- components/HelpPopup.jsx
+++ components/HelpPopup.jsx
@@ -32,14 +32,23 @@ export function HelpPopup() {
       <Dialog
         open={open}
         onClose={handleClose}
+        fullWidth={true}
+        maxWidth={"lg"}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">b4visとは</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
+            <div>b4visでは吹奏楽コンクールと夏の甲子園で上位大会に進んだ高校を可視化します。</div>
+            <div>左の①のビューでは、2013~2017年で以下のどちらか、または両方の結果を残した高校を表示しています。
+              左上のボタンから吹奏楽・野球ともに好成績の学校が多い/少ない県順に並び替えることができます。
+              <ul>
+                <li>吹奏楽コンクールで都道府県大会以上(東京・北海道は支部大会以上)の成績</li>
+                <li>夏の甲子園で地区大会ベスト8以上(東京・北海道はベスト4以上)</li>
+              </ul>
+            </div>
+
           </DialogContentText>
         </DialogContent>
       </Dialog>
