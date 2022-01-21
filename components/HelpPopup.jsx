76808d5c7a466e2040aa067ea884573805e317f0
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2022-01-21 23:50:14 +0900

    ダイアログの追記

--- components/HelpPopup.jsx
+++ components/HelpPopup.jsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
-  Button,
   Dialog,
-  DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
@@ -34,21 +32,64 @@ export function HelpPopup() {
         onClose={handleClose}
         fullWidth={true}
         maxWidth={"lg"}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">b4visとは</DialogTitle>
+        <DialogTitle style={{ fontSize: "1.5rem", fontWeight: "bolder" }}>
+          b4visとは
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            <div>b4visでは吹奏楽コンクールと夏の甲子園で上位大会に進んだ高校を可視化します。</div>
-            <div>左の①のビューでは、2013~2017年で以下のどちらか、または両方の結果を残した高校を表示しています。
-              左上のボタンから吹奏楽・野球ともに好成績の学校が多い/少ない県順に並び替えることができます。
-              <ul>
-                <li>吹奏楽コンクールで都道府県大会以上(東京・北海道は支部大会以上)の成績</li>
-                <li>夏の甲子園で地区大会ベスト8以上(東京・北海道はベスト4以上)</li>
-              </ul>
+          <DialogContentText>
+            <div>
+              b4visでは吹奏楽コンクールと夏の甲子園で上位大会に進んだ高校を可視化します。
             </div>
 
+            <div style={{padding:"1.5rem 0 1.5rem 0"}}>
+              <div>
+                左の①のビューでは、2013~2017年で以下のどちらか、または両方の結果を残した高校を表示しています。
+                左上のボタンから吹奏楽・野球ともに上位大会に進んだ学校が多い/少ない県順に並び替えることができます。
+                <ul style={{margin:"0.5rem 0 0.5rem 0"}}>
+                  <li>
+                    吹奏楽コンクールで都道府県大会以上(東京・北海道は支部大会以上)の成績
+                  </li>
+                  <li>
+                    夏の甲子園で地区大会ベスト8以上(東京・北海道はベスト4以上)
+                  </li>
+                </ul>
+              </div>
+              <div style={{padding:"1rem 0 1rem 0"}}>
+                グラフのセルをクリックすることでその学校のある都道府県の2013~2017年の詳細(ビュー②)とその学校の詳細(ビュー③)を見ることができます。
+              </div>
+              <div>
+                学校によってはデータが取れず不明の年もありますがご了承ください。
+              </div>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <img
+                  style={{ width: "50%", margin: "1rem" }}
+                  src={"./images/sample.png"}
+                  alt="サンプル"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div>データ提供</div>
+              <div>
+                吹奏楽：
+                <a
+                  href="https://www.musicabella.jp/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Musica Bella
+                </a>
+              </div>
+              {/* TODO:まるくんに聞く */}
+              <div>
+                甲子園：
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  甲子園
+                </a>
+              </div>
+            </div>
           </DialogContentText>
         </DialogContent>
       </Dialog>
