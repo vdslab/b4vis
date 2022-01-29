221db765a7c36d6c7989665176a118a7ab8a9709
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2022-01-30 02:42:19 +0900

    add ogp png

---
+++ next-seo.config.js
@@ -0,0 +1,29 @@
+export default {
+  defaultTitle: "b4vis",
+  description:
+    "吹奏楽コンクールと夏の甲子園で上位大会に進んだ高校を見ることができます",
+  canonical: "https://vdslab-b4vis.vercel.app",
+  additionalMetaTags: [
+    {
+      property: "dc:creator",
+      content: "運営者名",
+    },
+    {
+      name: "application-name",
+      content: "サイト名",
+    },
+  ],
+  openGraph: {
+    url: "https://vdslab-b4vis.vercel.app",
+    type: "website",
+    locale: "ja_JP",
+    site_name: "b4vis",
+    images: [
+      {
+        url: "https://vdslab-b4vis.vercel.app/images/ogp.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+};
